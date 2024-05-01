dda84f3f1796d5ecfaed63be974977d7d8233e39
Raqiee <raquelshortt@gmail.com>
2024-05-01 13:22:03 -0400

    saving docs

--- R/save_graphs_pep.R
+++ R/save_graphs_pep.R
@@ -1,5 +1,4 @@
 #' save_graphs_pep
-#'
 #' @param df_in_pep Bar graphs corresponding to peptides that had quantifiable
 #' extent of modification
 #' @param file_output The file path to save the graphs
@@ -9,12 +8,11 @@
 #' quantifiable extent of modification
 #' @export
 #'
-#' @examples
+#' @examples save_graphs_pep(df_in_pep, file_output, excel_filename)
 #' @aliases save_pep_graphs
-save_graphs_pep <- function(df_in_pep) {
-  for (protein in df_in_pep$MasterProteinAccessions) {
-    # subset the dataframe for this protein
-    temp <- subset(df_in_pep, MasterProteinAccessions == protein)
+save_graphs_pep <- function(df, file_output, excel_filename) {
+  for (protein in unique(df$MasterProteinAccessions)) {
+    temp <- subset(df, MasterProteinAccessions == protein)
     generate_eom_plot_pep(temp, file_output, excel_filename)
   }
 }
