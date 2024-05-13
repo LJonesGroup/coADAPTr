fa62316b5e7236d0028e9cc01a4e8e3681f1c6c8
Raqiee <raquelshortt@gmail.com>
2024-05-13 15:32:46 -0400

    updated all precursor function for TMT data before area_calculations_pep. resume there tomorrow

--- R/inst/coADAPTrTMPDInputT.R
+++ R/inst/coADAPTrTMPDInputT.R
@@ -166,87 +166,31 @@ rename_columns_interactively <- function(data) {
 
 renamed_data <- rename_columns_interactively(cleaned_data)
 
-#extracting proetin sequence
-#adding L or NL to files
-#extracting modification
-# Troubleshooting
-# List of column names containing "Abundance"
-abundance_cols <- grep("Abundance", colnames(pd_data), value = TRUE)
+# Transform the data in the column into rows
 
-# Define a function to convert to numeric with NAs as 0
-convert_to_numeric <- function(x) {
-  ifelse(is.na(x), 0, as.numeric(x))
+library(tidyr)
+
+transform_data <- function(data) {
+  # Pivot the data into long format, splitting columns containing ":"
+  transformed_data <- pivot_longer(data, cols = contains(":"), names_to = "Spectrum File", values_to = "Precursor Abundance")
+
+  return(transformed_data)
 }
 
-# Apply the function to abundance columns
-pd_data[, abundance_cols] <- lapply(pd_data[, abundance_cols], convert_to_numeric)
+transformed_data <- transform_data(renamed_data)
 
-# Now you can perform the calculations
-# Example: Sum of Abundance: 126 and Abundance: 127N columns
-pd_data$SumRow_Abundances <- rowSums(pd_data[, c("Abundance: 126", "Abundance: 127N", "Abundance: 127C", "Abundance: 128N", "Abundance: 128C", "Abundance: 129N", "Abundance: 129C", "Abundance: 130C", "Abundance: 131N", "Abundance: 131C")], na.rm = TRUE)
-#Assign new column names here.
-#Label which TMT channel corresponds to which sample by changing the column name (pd_data$*****)
-pd_data$VehicleL1 <- (pd_data$"Abundance: 126" * pd_data$Intensity )/ pd_data$SumRow_Abundances
-pd_data$VehicleL2 <- (pd_data$"Abundance: 127N" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-pd_data$VehicleL3 <- (pd_data$"Abundance: 127C" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-pd_data$VehicleNL1 <- (pd_data$"Abundance: 128N" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-pd_data$VehicleNL1 <- (pd_data$"Abundance: 128C" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-#pd_data$VehicleNL3 <- (pd_data$"Abundance: 129" * pd_data$Intensity ) / pd_data$SumRow_Abundances      #Not used in test data
-pd_data$DrugL1 <- (pd_data$"Abundance: 129N" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-pd_data$DrugL2 <- (pd_data$"Abundance: 129C" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-pd_data$DrugL3 <- (pd_data$"Abundance: 130C" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-pd_data$DrugNL1 <- (pd_data$"Abundance: 131N" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-pd_data$DrugNL2 <- (pd_data$"Abundance: 131C" * pd_data$Intensity ) / pd_data$SumRow_Abundances
-#pd_data$DrugnNL3 <- (pd_data$"Abundance: 130N" * pd_data$Intensity ) / pd_data$SumRow_Abundances
+#Identify Sample and Control Files
+SampleControl <- function(pd_data) {
+  pd_data$SampleControl <- ifelse(grepl("NL", pd_data$`Spectrum File`), "Control", "Sample")
+  return(pd_data)
+}
+transformed_data <- SampleControl(transformed_data)
 
-#Remove Old Columns
-columns_to_remove <- c(
-  "Abundance: 126",
-  "Abundance: 127N",
-  "Abundance: 127C",
-  "Abundance: 128N",
-  "Abundance: 128C",
-  "Abundance: 129N",
-  "Abundance: 129C",
-  "Abundance: 130C",
-  "Abundance: 131N",
-  "Abundance: 131C",
-  "SumRow_Abundances",
-  "Intensity"
-)
-#Transform abundances to rows for further pre processing and then FPOP Calculations
-# Remove the specified columns
-PD_data <- pd_data %>%
-  select(-one_of(columns_to_remove))%>%
-  gather(key = "Condition", value = "PrecursorAbundance", starts_with("VehicleL"), starts_with("DrugL"), starts_with("VehicleNL"), starts_with("DrugNL"))
-
-
-
-
-#Does this need to have a separate function or can it be merged with Annotate Features.
-# Annotate Sample/Control based on MS acquisitoin filename convention
-PD_data$SampleControl <- ifelse(PD_data$'Condition' %like%
-                                  "NL", "Control", "Sample")
-
-
-
-#Map acquisition file names
-###########################################################################
-# function to clean and parse data from PD output file
-annotate_features <- function(raw_data){
-
-  # Changing column header to match FASTA file
+#Annotate features in transformed data
+annotate_features <- function(raw_data) {
   raw_data <- raw_data %>%
-    rename('UniprotID' = 'Protein Accessions')
-
-
-  #Remove Conserved Proteins
-  raw_data<- raw_data%>%
-    filter(!grepl(";", raw_data$UniprotID)
-    )
-
-
-  #Remove Carbamidomethyls
+    mutate(`Master Protein Accessions` = sapply(strsplit(`Master Protein Accessions`, ";"), `[`, 1),
+           UniprotID = `Master Protein Accessions`)  # Adding this line to create the UniprotID column
 
   raw_data$Modifications <- gsub(";C\\d+\\(Carbamidomethyl\\)", ";", raw_data$Modifications)
   raw_data$Modifications <- gsub("C\\d+\\(Carbamidomethyl\\);", ";", raw_data$Modifications)
@@ -256,20 +200,10 @@ annotate_features <- function(raw_data){
   raw_data$Modifications <- ifelse(grepl("^; [A-Z]", raw_data$Modifications),
                                    sub("^;\\s", "", raw_data$Modifications),
                                    raw_data$Modifications)
-
-
-  #remove TMT tag from modifications columnn
-#Separate TMT Modifications
-
-
-
-  #Create MODC olumn to distinguish Oxidized vs UnOxidized
   raw_data$MOD <- ifelse(is.na(raw_data$Modifications) | raw_data$Modifications == "", "Unoxidized",
                          ifelse(grepl("()", raw_data$Modifications), "Oxidized", "Unoxidized"))
   raw_data$MOD <- ifelse(is.na(raw_data$Modifications) | raw_data$Modifications == "" | grepl("^\\s*$", raw_data$Modifications) | !grepl("[A-Za-z]", raw_data$Modifications), "Unoxidized", raw_data$MOD)
 
-
-  # Mod Position*
   raw_data$ModPositionL <- sub("^([[:alpha:]]*).*", "\\1",
                                raw_data$Modifications)
   raw_data$ModPositionN <- as.numeric(gsub(".*?([0-9]+).*", "\\1",
@@ -280,48 +214,47 @@ annotate_features <- function(raw_data){
                                        raw_data$ModPositionN))
   raw_data$ModPosition <- gsub("^; ", "", raw_data$ModPosition)
 
+  return(raw_data)
+}
+
+annotated_data<-annotate_features(transformed_data)
+
+#Parse the FASTA file
+
+parse_fasta <- function(fasta_in){
+  fasta_in <- fasta_in %>%
+    rename(protein_sequence = seq.text)
+  fasta_in<- fasta_in %>%
+    rename(MasterProteinAccessions = seq.name)
+
+  fasta_in$UniprotID <- gsub("^.+\\|(\\w+)\\|.*$", "\\1", fasta_in$MasterProteinAccessions)
+
+  return(fasta_in)
+
+}
+FASTA<- parse_fasta(FASTA)
+
+#Merge the FASTA file with the annotated data
+locate_startend_res <- function(raw_data){
+
+  uniqueMPA <- unique(raw_data[, c("Master Protein Accessions",'Sequence')])
+  uniqueMPA <- as.data.frame(uniqueMPA)
+
+  raw_data <- merge(raw_data, FASTA, by = "UniprotID")
+
+  index <- str_locate(raw_data$protein_sequence, raw_data$Sequence)
+  raw_data <- cbind(raw_data, index)  # TODO: memory efficiency?
+
+  raw_data$peptide<- paste(raw_data$start,"-", raw_data$end)
+
+  raw_data$mod_count <- str_count(raw_data$Modifications, "\\(.*?\\)")
+  raw_data$mod_count <- ifelse(raw_data$MOD == "Unoxidized", 0, raw_data$mod_count)
+
+  raw_data$mod_res <- ifelse(raw_data$ModPositionN>0, raw_data$start + raw_data$ModPositionN - 1, NA)
+
+  raw_data$Res<- paste(raw_data$ModPositionL, raw_data$mod_res)
 
   return(raw_data)
 }
 
-# parse the PD data
-pd_data_annotated <- annotate_features(PD_data)
-
-
-#Test
-# Updated annotate_features function
-annotate_features <- function(raw_data){
-  # Changing column header to match FASTA file
-  raw_data <- raw_data %>%
-    rename('UniprotID' = 'Protein Accessions')
-
-  # Remove Conserved Proteins
-  raw_data <- raw_data %>%
-    filter(!grepl(";", raw_data$UniprotID))
-
-  # Remove TMT and Carbamidomethyl modifications and their parentheses and surrounding characters
-  raw_data$Modifications <- gsub("\\b(TMT\\w*|Carbamidomethyl\\w*);?\\s*|[(]\\w+\\([^)]*\\)[)];?\\s*", "", raw_data$Modifications)
-
-  # Remove any leading or trailing semicolons or spaces
-  raw_data$Modifications <- gsub("^;\\s*|\\s*;$", "", raw_data$Modifications)
-
-  # Create MOD column to distinguish Oxidized vs UnOxidized
-  raw_data$MOD <- ifelse(
-    is.na(raw_data$Modifications) | raw_data$Modifications == "", "Unoxidized",
-    ifelse(grepl("()", raw_data$Modifications), "Oxidized", "Unoxidized")
-  )
-  raw_data$MOD <- ifelse(
-    is.na(raw_data$Modifications) | raw_data$Modifications == "" | grepl("^\\s*$", raw_data$Modifications) | !grepl("[A-Za-z]", raw_data$Modifications), "Unoxidized", raw_data$MOD)
-
-  # Mod Position
-  raw_data$ModPositionL <- sub("^([[:alpha:]]*).*", "\\1", raw_data$Modifications)
-  raw_data$ModPositionN <- as.numeric(gsub(".*?([0-9]+).*", "\\1", raw_data$Modifications))
-  raw_data$ModPosition <- ifelse(raw_data$ModPositionL == "NA", "NA", paste(raw_data$ModPositionL, raw_data$ModPositionN))
-  raw_data$ModPosition <- gsub("^; ", "", raw_data$ModPosition)
-
-  return(raw_data)
-}
-
-# Parse the PD data
-pd_data_annotated <- annotate_features(PD_data)
-
+pd_data_fasta_merged <- locate_startend_res(annotated_data)
