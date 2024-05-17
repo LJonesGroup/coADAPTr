60a6f56e8b5b0a4e0a372ea152dc14968e173c36
Raqiee <raquelshortt@gmail.com>
2024-05-17 14:59:09 -0400

    fixed area calculations testing sd from Haolin

--- R/annotate_features.R
+++ R/annotate_features.R
@@ -12,39 +12,34 @@ annotate_features <- function(raw_data) {
     mutate(`Master Protein Accessions` = sapply(strsplit(`Master Protein Accessions`, ";"), `[`, 1),
            UniprotID = `Master Protein Accessions`)  # Adding this line to create the UniprotID column
 
-  # Rename Modifications column to Mods
   raw_data <- raw_data %>%
     rename(Mods = Modifications)
 
-  # Remove specific modifications and clean up semicolons
   raw_data <- raw_data %>%
     mutate(Modifications = Mods) %>%
     mutate(Modifications = gsub(";[A-Z]\\d+\\(Carbamidomethyl\\)", ";", Modifications)) %>%
     mutate(Modifications = gsub("[A-Z]\\d+\\(Carbamidomethyl\\);", ";", Modifications)) %>%
-    mutate(Modifications = gsub("[A-Z]\\d+\\(Carbamidomethyl\\)", "", Modifications)) %>%
-    mutate(Modifications = gsub(";[A-Z]\\d+\\(TMT[^)]*\\)", ";", Modifications)) %>%
-    mutate(Modifications = gsub("[A-Z]\\d+\\(TMT[^)]*\\);", ";", Modifications)) %>%
-    mutate(Modifications = gsub("[A-Z]\\d+\\(TMT[^)]*\\)", "", Modifications))
+    mutate(Modifications = gsub("[A-Z]\\d+\\(Carbamidomethyl\\)", "", Modifications))
 
-  # Cleanup: Remove leading, trailing, and multiple consecutive semicolons
+
   raw_data <- raw_data %>%
     mutate(Modifications = gsub("\\s*;\\s*", ";", Modifications)) %>%  # Remove spaces around semicolons
     mutate(Modifications = gsub(";{2,}", ";", Modifications)) %>%  # Replace multiple semicolons with a single one
     mutate(Modifications = gsub("^;|;$", "", Modifications))  # Remove leading and trailing semicolons
 
-  # Determine if the sequence is oxidized or unoxidized
   raw_data <- raw_data %>%
     mutate(MOD = ifelse(is.na(Modifications) | Modifications == "", "Unoxidized",
                         ifelse(grepl("", Modifications), "Oxidized", "Unoxidized")))
 
-  # Create ModPositionL and ModPositionN columns
   raw_data <- raw_data %>%
     mutate(ModPositionL = sub("^([A-Z]*).*", "\\1", Modifications)) %>%
     mutate(ModPositionN = gsub(".*?([0-9]+).*", "\\1", Modifications)) %>%
     mutate(ModPositionN = ifelse(ModPositionN == Modifications, NA, ModPositionN)) %>%
+    mutate(ModPositionN = as.numeric(ModPositionN)) %>%  # Ensure ModPositionN is numeric
     mutate(ModPosition = ifelse(is.na(ModPositionL) | ModPositionL == "", NA,
                                 paste(ModPositionL, ModPositionN, sep = "")))
 
   return(raw_data)
 }
+
 
