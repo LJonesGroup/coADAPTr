b1fdee3a94cc89b47a1ab6b4457d70216656784f
Raqiee <raquelshortt@gmail.com>
2024-05-22 01:21:47 -0400

    added mod per protein counts

--- R/inst/coADAPTrTMPDInputT.R
+++ R/inst/coADAPTrTMPDInputT.R
@@ -256,9 +256,6 @@ parse_fasta <- function(fasta_in){
 FASTA<- parse_fasta(FASTA)
 
 #Merge the FASTA file with the annotated data
-library(dplyr)
-library(stringr)
-
 locate_startend_res <- function(raw_data, FASTA){
 
   # Merge raw_data with FASTA by UniprotID
@@ -725,3 +722,215 @@ generate_grouped_bar_plot_res <- function() {
 }
 
 generate_grouped_bar_plot_res()
+
+#Generate Venn Diagrams
+venn_diagram <- function() {
+  # Prompt user to select Excel file
+  excel_file <- file.choose()
+
+  # Read data from Excel file
+  data <- read_excel(excel_file, col_names = TRUE)
+
+  # Prompt user to select output folder
+  output_folder <- choose.dir()
+
+  # Get the number of conditions
+  num_conditions <- ncol(data)
+
+  # Get bubble names from the user
+  venn_bubble_names <- readline(prompt = "Enter names for the Venn diagram bubbles separated by commas: ")
+  venn_bubble_names <- strsplit(venn_bubble_names, ",")[[1]]
+
+  # Ensure the number of bubble names matches the number of conditions
+  if (length(venn_bubble_names) != num_conditions) {
+    stop("Number of bubble names provided does not match the number of columns in the Excel file.")
+  }
+
+  # Process each column separately
+  condition_lists <- lapply(seq_len(num_conditions), function(i) {
+    column_data <- na.omit(data[[i]])
+    if (length(column_data) == 0) {
+      return(NULL)
+    } else {
+      return(column_data)
+    }
+  })
+
+  # Generate a custom color palette (using shades of blue or gray)
+  custom_palette <- c("#0570b0", "#3690c0", "#74a9cf", "#a6bddb", "#d0d1e6", "#084594", "#2171b5", "#4292c6", "#6baed6", "#9ecae1", "#c6dbef", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef")
+  custom_palette <- custom_palette[1:num_conditions] # ensuring the palette has the desired number of colors
+
+  # Create Venn diagram plot
+  venn_plot <- venn.diagram(
+    x = condition_lists,
+    category.names = venn_bubble_names,
+    filename = NULL,
+    col = custom_palette,
+    fill = custom_palette,
+    alpha = 0.5, # Adjust transparency for better visualization
+    margin = 0.1, # Increase margin for better plot appearance
+    fontfamily = "sans", # Set font family to Helvetica (or similar font)
+    fontface = "bold",   # Set font face to bold
+    cat.fontsize = 20,   # Set category font size to 20
+    cex = 2.5,           # Adjust overall font size
+    cat.cex = 2.5,         # Set category title font size to 24
+    cat.fontfamily = "sans", # Set category title font family
+    cat.fontface = "bold"  # Set category title font face to bold
+  )
+
+  # Prompt user to specify the name of the output PNG file
+  png_file_name <- readline(prompt = "Enter the name of the output PNG file (without extension): ")
+  png_file <- file.path(output_folder, paste0(png_file_name, ".png"))
+
+  # Save plot as PNG
+  png(filename = png_file, width = 1000, height = 1000) # Adjust width and height as needed
+  grid.draw(venn_plot)
+  dev.off()
+
+  cat("Venn diagram plot saved at:", png_file, "\n")
+
+  # Calculate overlap
+  overlap <- calculate.overlap(condition_lists)
+
+  # Save overlap to Excel
+  overlap_file <- file.path(output_folder, paste0(png_file_name, "_Overlap.xlsx"))
+  write.xlsx(overlap, overlap_file)
+
+  cat("Overlap information saved at:", overlap_file, "\n")
+
+  if (FALSE) {
+    venn_diagram()
+  }
+}
+venn_diagram()
+
+#####
+#Counting modified peptides per protein and graphing
+
+count_peptides_per_protein <- function() {
+  data_list <- list()
+  conditions <- c()
+
+  repeat {
+    file_path <- file.choose()
+    condition <- readline(prompt = "What condition does this data correspond to? ")
+    conditions <- c(conditions, condition)
+
+    data <- read_excel(file_path)
+
+    count_data <- data %>%
+      group_by(MasterProteinAccessions) %>%
+      summarise(SequenceCount = n()) %>%
+      ungroup()
+
+    data_list[[condition]] <- count_data
+
+    more_files <- readline(prompt = "Do you want to input another file? (yes/no) ")
+    if (tolower(more_files) != "yes") {
+      break
+    }
+  }
+
+  graph_title <- readline(prompt = "Enter the title for the Bar Graph: ")
+
+  combined_data <- bind_rows(data_list, .id = "Condition")
+  summary_data <- combined_data %>%
+    group_by(Condition, SequenceCount) %>%
+    summarise(ProteinCount = n(), .groups = 'drop')
+
+  p <- ggplot(summary_data, aes(x = SequenceCount, y = ProteinCount, fill = Condition)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    labs(title = graph_title, x = "Number of Peptides per Protein", y = "Number of Proteins") +
+    theme_minimal(base_size = 15) +
+    theme(panel.background = element_rect(fill = "white", colour = NA),
+          plot.background = element_rect(fill = "white", colour = NA),
+          legend.background = element_rect(fill = "white", colour = NA),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          plot.title = element_text(hjust = 0.5, size = 20))
+
+  print(p)
+
+  output_dir <- choose.dir(default = "", caption = "Select directory to save the count data and graph")
+  file_name <- readline(prompt = "Enter the base name for the saved files (without extension): ")
+
+  output_file <- file.path(output_dir, paste0(file_name, ".xlsx"))
+  graph_file <- file.path(output_dir, paste0(file_name, ".png"))
+
+  write_xlsx(summary_data, output_file)
+  ggsave(graph_file, plot = p, width = 10, height = 6)
+
+  print(paste("Data saved to", output_file))
+  print(paste("Graph saved to", graph_file))
+}
+
+# To run the function
+count_peptides_per_protein()
+####
+
+
+#Count Residues Per Protein
+count_residue_entries_per_protein <- function() {
+  data_list <- list()
+  conditions <- c()
+
+  repeat {
+    file_path <- file.choose()
+    condition <- readline(prompt = "What condition does this data correspond to? ")
+    conditions <- c(conditions, condition)
+
+    data <- read_excel(file_path)
+
+    # Ensure "Res" column contains non-NA values and only valid entries with numbers followed by letters
+    data <- data %>% filter(!is.na(Res) & grepl("^[A-Za-z][0-9]+", Res))
+
+    count_data <- data %>%
+      group_by(MasterProteinAccessions) %>%
+      summarise(ResidueEntryCount = n()) %>%
+      ungroup()
+
+    data_list[[condition]] <- count_data
+
+    more_files <- readline(prompt = "Do you want to input another file? (yes/no) ")
+    if (tolower(more_files) != "yes") {
+      break
+    }
+  }
+
+  graph_title <- readline(prompt = "Enter the title for the Bar Graph: ")
+
+  combined_data <- bind_rows(data_list, .id = "Condition")
+  summary_data <- combined_data %>%
+    group_by(Condition, ResidueEntryCount) %>%
+    summarise(ProteinCount = n(), .groups = 'drop')
+
+  p <- ggplot(summary_data, aes(x = ResidueEntryCount, y = ProteinCount, fill = Condition)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    labs(title = graph_title, x = "Number of Residue Entries per Protein", y = "Number of Proteins") +
+    theme_minimal(base_size = 15) +
+    theme(panel.background = element_rect(fill = "white", colour = NA),
+          plot.background = element_rect(fill = "white", colour = NA),
+          legend.background = element_rect(fill = "white", colour = NA),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          plot.title = element_text(hjust = 0.5, size = 20))
+
+  print(p)
+
+  output_dir <- choose.dir(default = "", caption = "Select directory to save the count data and graph")
+  file_name <- readline(prompt = "Enter the base name for the saved files (without extension): ")
+
+  output_file <- file.path(output_dir, paste0(file_name, ".xlsx"))
+  graph_file <- file.path(output_dir, paste0(file_name, ".png"))
+
+  write_xlsx(summary_data, output_file)
+  ggsave(graph_file, plot = p, width = 10, height = 6)
+
+  print(paste("Data saved to", output_file))
+  print(paste("Graph saved to", graph_file))
+}
+
+# To run the function
+count_residue_entries_per_protein()
