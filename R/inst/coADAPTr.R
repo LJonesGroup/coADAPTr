6ff5d92dc92a2028cfdc5a3703221248250460ba
Raqiee <raquelshortt@gmail.com>
2024-07-18 22:48:10 -0400

    updated other functions

--- R/inst/coADAPTr.R
+++ R/inst/coADAPTr.R
@@ -418,106 +418,6 @@ Areas_res<- area_calculations_resss(pd_data_fasta_merged)
 
 
 
-#####TESTING RES LEVEL SD
-
-#Calculate the Total Variation
-# Calculate Total Variance
-
-######################
-#Calculate Variance and Standard Variation
-# Group by the necessary columns including 'Condition'
-RESSDTEST <- function(df_in) {
-  df_out<- df_in %>%
-    filter((mod_count == 0 | mod_count == 1) & MOD == "Oxidized") %>%
-    group_by(MasterProteinAccessions, Sequence, Res, SampleControl, Condition) %>%
-    summarize(OxidizedVar = var(`Precursor Abundance`, na.rm = TRUE), .groups = "drop") %>%
-    pivot_wider(
-      id_cols = c("MasterProteinAccessions", "Sequence", "Res", "Condition"),
-      names_from = c("SampleControl"),
-      values_from = "OxidizedVar",
-      values_fill = NA
-    )
-  # Rename columns
-  df_out <- df_out %>%
-  rename(
-    Control_OxidizedVar = Control,
-    Sample_OxidizedVar = Sample,
-
-    )
-  #Calculating total variance
-
-
-  # Return the final dataframe
-  return(df_out)
-}
-sdtest<- RESSDTEST(pd_data_fasta_merged)
-
-###UNOXIDIZED TEST
-RESSDTESTall <- function(df_in) {
-  df_out <- df_in %>%
-    filter(mod_count == 0 | mod_count == 1) %>%
-    group_by(MasterProteinAccessions, Sequence, SampleControl, MOD, Condition) %>%
-    summarize(TotalArea = var(`Precursor Abundance`, na.rm = TRUE), .groups = "drop") %>%
-    pivot_wider(
-      id_cols = c("MasterProteinAccessions", "Sequence", "Condition"),
-      names_from = c("SampleControl", "MOD"),
-      values_from = "TotalArea",
-      values_fill = NA
-    )
-  # Rename columns
-  df_out <- df_out %>%
-    rename(
-      Control_OxidizedVar = Control,
-      Sample_OxidizedVar = Sample,
-
-    )
-
-
-  # Return the final dataframe
-  return(df_out)
-}
-sdtestunox_all<- RESSDTESTall(pd_data_fasta_merged)
-
-####
-#merge of total and ox variance
-merged<- inner_join(sdtestunox_all, sdtest, by = c("MasterProteinAccessions", "Sequence", "Condition"))
-#TESTING RES SD
-RESSDTESTTOTALS <- function(df_in) {
-  # Calculate Total Variance
-  df_out <- df_in %>%
-    filter((mod_count == 0 | mod_count == 1)) %>%
-    group_by(MasterProteinAccessions, Sequence, SampleControl, Condition) %>%
-    summarize(TotalVar = var(`Precursor Abundance`, na.rm = TRUE), .groups = "drop") %>%
-    pivot_wider(
-      id_cols = c("MasterProteinAccessions", "Sequence", "Condition"),
-      names_from = c("SampleControl"),
-      values_from = "TotalVar",
-      values_fill = list(TotalVar = NA)
-    ) %>%
-    rename(
-      Sample_TotalVar = Sample,
-      Control_TotalVar = Control
-    )
-
-  return(df_out)
-}
-
-# Assuming pd_data_fasta_merged is your dataset
-sdtesttotals <- RESSDTESTTOTALS(pd_data_fasta_merged)
-
-
-
-
-
-
-
-
-
-
-# Assuming pd_data_fasta_merged is your dataset
-residue_data <- area_calculations_res(pd_data_fasta_merged)
-# Assuming pd_data_fasta_merged is your dataset
-residue_data <- area_calculations_res(pd_data_fasta_merged)
 
 # Step 14  Subset sequence metadata like residue start/stop for residue level data
 graphing_df_res<- graphing_data_res(Areas_res, pd_data_fasta_merged)
@@ -545,19 +445,359 @@ save_data_frames(file_output, TotalsTable = TotalsTable, quant_graph_df_pep = qu
 
 # Step 16C Saving Peptide Level Bar Graphs
 
-generate_eom_plot_pep(df_in = quant_graph_df_pep, file_output = file_output)
+generate_grouped_bar_plot_pep <- function() {
+  # Prompt the user to select the Excel file
+  cat("Select the Excel file containing the data: ")
+  filepath <- file.choose()
 
-#Step 16D Saving Residue Level Bar Graphs
+  # Load the data from the selected Excel file
+  df_in <- readxl::read_excel(filepath)
 
-generate_eom_plot_res(df_in = quant_graph_df_res, file_output = file_output)
+  # Arrange the dataframe by start
+  df_in <- df_in %>%
+    arrange(start)
 
-#Step 17 Saving Venn Diagrams
-venn_diagram()
+  # Auto-detect unique conditions
+  unique_conditions <- unique(df_in$Condition)
 
-#Step 18 Saving Peptide Level Grouped Bar Graphs
+  # Manually specify blue colors for filling the bars
+  condition_colors <- c("#0570b0", "#3690c0", "#74a9cf", "#a6bddb", "#d0d1e6", "#084594", "#2171b5", "#4292c6", "#6baed6", "#9ecae1", "#c6dbef", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef")
+
+  # Create a factor variable to represent the sorted order of conditions
+  df_in$Condition <- factor(df_in$Condition, levels = unique_conditions)
+
+  # Prompt the user to specify the filename
+  cat("Enter the filename (without extension): ")
+  filename <- readline()
+
+  # Remove any leading or trailing whitespace
+  filename <- trimws(filename)
+
+  # Iterate over each protein and make a grouped bar plot for it
+  for (protein in unique(df_in$MasterProteinAccessions)) {
+    # Subset the dataframe for this protein
+    temp <- subset(df_in, MasterProteinAccessions == protein)
+
+    # Generate a grouped bar plot of the extent of modification for each peptide
+    # that maps to this protein, with different conditions represented by color
+    fig <- ggplot(temp, aes(x = peptide, y = EOM, fill = Condition)) +
+      geom_bar(position = position_dodge(width = 0.9), stat = "identity") +
+      geom_errorbar(aes(ymin = EOM - SD, ymax = EOM + SD), position = position_dodge(width = 0.9), width = 0.25) +
+      labs(title = paste(protein, "Peptide Level Analysis"),
+           x = "Peptide",
+           y = "Extent of Modification",
+           fill = "Condition") +
+      theme_classic() +
+      theme(text = element_text(size = 18, family = "sans"),
+            plot.title = element_text(hjust = 0.5, size = 20, family = "sans", face = "bold"),
+            legend.text = element_text(size = 16, family = "sans"),
+            legend.title = element_text(size = 18, family = "sans"),
+            axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 1),
+            axis.title.x = element_text(margin = margin(t = 20))) +
+      scale_fill_manual(values = condition_colors)
+
+    # Create the output directory for bar graphs based on the file output and excel filename
+    graph_output_directory <- file.path(file_output, paste0(filename, "_PeptideLevelGroupedBarGraphs"))
+    dir.create(graph_output_directory, showWarnings = FALSE, recursive = TRUE)
+
+    # Generate the full file path for this protein and save the figure
+    file_out <- file.path(graph_output_directory, paste0(protein, ".png"))
+    ggsave(filename = file_out, plot = fig, device = "png")
+
+    # Print a message to indicate successful saving
+    cat("Grouped bar graph for", protein, "saved as", file_out, "\n")
+  }
+}
 
 generate_grouped_bar_plot_pep()
 
+
+
+
+#Step 16D Saving Residue Level Bar Graphs
+
+
+# Generating grouped bar plot at the residue level
+generate_grouped_bar_plot_res <- function() {
+  # Prompt the user to select the Excel file
+  cat("Select the Excel file containing the data: ")
+  filepath <- file.choose()
+
+  # Load the data from the selected Excel file
+  df_in <- readxl::read_excel(filepath)
+
+  # Arrange the dataframe by start
+  df_in <- df_in %>%
+    arrange(start)
+
+  # Auto-detect unique conditions
+  unique_conditions <- unique(df_in$Condition)
+
+  # Manually specify blue colors for filling the bars
+  condition_colors <- c("#0570b0", "#3690c0", "#74a9cf", "#a6bddb", "#d0d1e6", "#084594", "#2171b5", "#4292c6", "#6baed6", "#9ecae1", "#c6dbef", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef")
+
+  # Create a factor variable to represent the sorted order of conditions
+  df_in$Condition <- factor(df_in$Condition, levels = unique_conditions)
+
+  # Prompt the user to specify the filename
+  cat("Enter the filename (without extension): ")
+  filename <- readline()
+
+  # Remove any leading or trailing whitespace
+  filename <- trimws(filename)
+
+  # Iterate over each protein and make a grouped bar plot for it
+  for (protein in unique(df_in$MasterProteinAccessions)) {
+    # Subset the dataframe for this protein
+    temp <- subset(df_in, MasterProteinAccessions == protein)
+
+    # Generate a grouped bar plot of the extent of modification for each peptide
+    # that maps to this protein, with different conditions represented by color
+    fig <- ggplot(temp, aes(x = Res, y = EOM, fill = Condition)) +
+      geom_bar(position = position_dodge(width = 0.9), stat = "identity") +
+      geom_errorbar(aes(ymin = EOM - SD, ymax = EOM + SD), position = position_dodge(width = 0.9), width = 0.25) +
+      labs(title = paste(protein, "Residue Level Analysis"),
+           x = "Residue",
+           y = "Extent of Modification",
+           fill = "Condition") +
+      theme_classic() +
+      theme(text = element_text(size = 18, family = "sans"),
+            plot.title = element_text(hjust = 0.5, size = 20, family = "sans", face = "bold"),
+            legend.text = element_text(size = 16, family = "sans"),
+            legend.title = element_text(size = 18, family = "sans"),
+            axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 1),
+            axis.title.x = element_text(margin = margin(t = 20))) +
+      scale_fill_manual(values = condition_colors)
+
+    # Create the output directory for bar graphs based on the file output and excel filename
+    graph_output_directory <- file.path(file_output, paste0(filename, "_ResidueLevelGroupedBarGraphs"))
+    dir.create(graph_output_directory, showWarnings = FALSE, recursive = TRUE)
+
+    # Generate the full file path for this protein and save the figure
+    file_out <- file.path(graph_output_directory, paste0(protein, ".png"))
+    ggsave(filename = file_out, plot = fig, device = "png")
+
+    # Print a message to indicate successful saving
+    cat("Grouped bar graph for", protein, "saved as", file_out, "\n")
+  }
+}
+
 generate_grouped_bar_plot_res()
+
+#Step 17 Saving Venn Diagrams
+#Generate Venn Diagrams
+venn_diagram <- function() {
+  # Prompt user to select Excel file
+  excel_file <- file.choose()
+
+  # Read data from Excel file
+  data <- read_excel(excel_file, col_names = TRUE)
+
+  # Prompt user to select output folder
+  output_folder <- choose.dir()
+
+  # Get the number of conditions
+  num_conditions <- ncol(data)
+
+  # Get bubble names from the user
+  venn_bubble_names <- readline(prompt = "Enter names for the Venn diagram bubbles separated by commas: ")
+  venn_bubble_names <- strsplit(venn_bubble_names, ",")[[1]]
+
+  # Ensure the number of bubble names matches the number of conditions
+  if (length(venn_bubble_names) != num_conditions) {
+    stop("Number of bubble names provided does not match the number of columns in the Excel file.")
+  }
+
+  # Process each column separately
+  condition_lists <- lapply(seq_len(num_conditions), function(i) {
+    column_data <- na.omit(data[[i]])
+    if (length(column_data) == 0) {
+      return(NULL)
+    } else {
+      return(column_data)
+    }
+  })
+
+  # Generate a custom color palette (using shades of blue or gray)
+  custom_palette <- c("#0570b0", "#3690c0", "#74a9cf", "#a6bddb", "#d0d1e6", "#084594", "#2171b5", "#4292c6", "#6baed6", "#9ecae1", "#c6dbef", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef")
+  custom_palette <- custom_palette[1:num_conditions] # ensuring the palette has the desired number of colors
+
+  # Create Venn diagram plot
+  venn_plot <- venn.diagram(
+    x = condition_lists,
+    category.names = venn_bubble_names,
+    filename = NULL,
+    col = custom_palette,
+    fill = custom_palette,
+    alpha = 0.5, # Adjust transparency for better visualization
+    margin = 0.1, # Increase margin for better plot appearance
+    fontfamily = "sans", # Set font family to Helvetica (or similar font)
+    fontface = "bold",   # Set font face to bold
+    cat.fontsize = 20,   # Set category font size to 20
+    cex = 2.5,           # Adjust overall font size
+    cat.cex = 2.5,         # Set category title font size to 24
+    cat.fontfamily = "sans", # Set category title font family
+    cat.fontface = "bold"  # Set category title font face to bold
+  )
+
+  # Prompt user to specify the name of the output PNG file
+  png_file_name <- readline(prompt = "Enter the name of the output PNG file (without extension): ")
+  png_file <- file.path(output_folder, paste0(png_file_name, ".png"))
+
+  # Save plot as PNG
+  png(filename = png_file, width = 1000, height = 1000) # Adjust width and height as needed
+  grid.draw(venn_plot)
+  dev.off()
+
+  cat("Venn diagram plot saved at:", png_file, "\n")
+
+  # Calculate overlap
+  overlap <- calculate.overlap(condition_lists)
+
+  # Save overlap to Excel
+  overlap_file <- file.path(output_folder, paste0(png_file_name, "_Overlap.xlsx"))
+  write.xlsx(overlap, overlap_file)
+
+  cat("Overlap information saved at:", overlap_file, "\n")
+
+  if (FALSE) {
+    venn_diagram()
+  }
+}
+venn_diagram()
+
+
+#Step 18 Saving Modifications Per Peptide and Residue
+
+#Counting modified peptides per protein and graphing
+
+count_peptides_per_protein <- function() {
+  data_list <- list()
+  conditions <- c()
+
+  repeat {
+    file_path <- file.choose()
+    condition <- readline(prompt = "What condition does this data correspond to? ")
+    conditions <- c(conditions, condition)
+
+    data <- read_excel(file_path)
+
+    count_data <- data %>%
+      group_by(MasterProteinAccessions) %>%
+      summarise(SequenceCount = n()) %>%
+      ungroup()
+
+    data_list[[condition]] <- count_data
+
+    more_files <- readline(prompt = "Do you want to input another file? (yes/no) ")
+    if (tolower(more_files) != "yes") {
+      break
+    }
+  }
+
+  graph_title <- readline(prompt = "Enter the title for the Bar Graph: ")
+
+  combined_data <- bind_rows(data_list, .id = "Condition")
+  summary_data <- combined_data %>%
+    group_by(Condition, SequenceCount) %>%
+    summarise(ProteinCount = n(), .groups = 'drop')
+
+  p <- ggplot(summary_data, aes(x = SequenceCount, y = ProteinCount, fill = Condition)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    labs(title = graph_title, x = "Number of Peptides per Protein", y = "Number of Proteins") +
+    theme_minimal(base_size = 15) +
+    theme(panel.background = element_rect(fill = "white", colour = NA),
+          plot.background = element_rect(fill = "white", colour = NA),
+          legend.background = element_rect(fill = "white", colour = NA),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          plot.title = element_text(hjust = 0.5, size = 20))
+
+  print(p)
+
+  output_dir <- choose.dir(default = "", caption = "Select directory to save the count data and graph")
+  file_name <- readline(prompt = "Enter the base name for the saved files (without extension): ")
+
+  output_file <- file.path(output_dir, paste0(file_name, ".xlsx"))
+  graph_file <- file.path(output_dir, paste0(file_name, ".png"))
+
+  write_xlsx(summary_data, output_file)
+  ggsave(graph_file, plot = p, width = 10, height = 6)
+
+  print(paste("Data saved to", output_file))
+  print(paste("Graph saved to", graph_file))
+}
+
+# To run the function
+count_peptides_per_protein()
+####
+
+
+#Count Residues Per Protein
+count_residue_entries_per_protein <- function() {
+  data_list <- list()
+  conditions <- c()
+
+  repeat {
+    file_path <- file.choose()
+    condition <- readline(prompt = "What condition does this data correspond to? ")
+    conditions <- c(conditions, condition)
+
+    data <- read_excel(file_path)
+
+    # Ensure "Res" column contains non-NA values and only valid entries with numbers followed by letters
+    data <- data %>% filter(!is.na(Res) & grepl("^[A-Za-z][0-9]+", Res))
+
+    count_data <- data %>%
+      group_by(MasterProteinAccessions) %>%
+      summarise(ResidueEntryCount = n()) %>%
+      ungroup()
+
+    data_list[[condition]] <- count_data
+
+    more_files <- readline(prompt = "Do you want to input another file? (yes/no) ")
+    if (tolower(more_files) != "yes") {
+      break
+    }
+  }
+
+  graph_title <- readline(prompt = "Enter the title for the Bar Graph: ")
+
+  combined_data <- bind_rows(data_list, .id = "Condition")
+  summary_data <- combined_data %>%
+    group_by(Condition, ResidueEntryCount) %>%
+    summarise(ProteinCount = n(), .groups = 'drop')
+
+  p <- ggplot(summary_data, aes(x = ResidueEntryCount, y = ProteinCount, fill = Condition)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    labs(title = graph_title, x = "Number of Residue Entries per Protein", y = "Number of Proteins") +
+    theme_minimal(base_size = 15) +
+    theme(panel.background = element_rect(fill = "white", colour = NA),
+          plot.background = element_rect(fill = "white", colour = NA),
+          legend.background = element_rect(fill = "white", colour = NA),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          plot.title = element_text(hjust = 0.5, size = 20))
+
+  print(p)
+
+  output_dir <- choose.dir(default = "", caption = "Select directory to save the count data and graph")
+  file_name <- readline(prompt = "Enter the base name for the saved files (without extension): ")
+
+  output_file <- file.path(output_dir, paste0(file_name, ".xlsx"))
+  graph_file <- file.path(output_dir, paste0(file_name, ".png"))
+
+  write_xlsx(summary_data, output_file)
+  ggsave(graph_file, plot = p, width = 10, height = 6)
+
+  print(paste("Data saved to", output_file))
+  print(paste("Graph saved to", graph_file))
+}
+
+# To run the function
+count_residue_entries_per_protein()
+
 
 dev.off()
