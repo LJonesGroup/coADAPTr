ae3dcaaa97871201e62e6ec7130814aec48ea3ec
crojaram <crojaram@umich.edu>
2024-12-12 18:58:32 -0500

    Adapting import data to DIA file

--- R/import_data.R
+++ R/import_data.R
@@ -15,11 +15,19 @@ import_data <- function() {
   # Detecting file extension for proper data frame creation
   filename <- basename(file_path)
   split_result <- strsplit(filename, "\\.")[[1]]
+  filename <- split_result[1]
   extension <- split_result[2]
 
   if(extension == "tsv"){
     print("Input file is tab-delim type")
-    df <- read.csv(file_path, sep = "\t",check.names = FALSE)
+    if(filename == "psm"){
+      print("Analysing psm.tsv file (FragPipe-DDA)")
+      df <- read.csv(file_path, sep = "\t",check.names = FALSE)
+    }else if(filename == "report"){
+      print("Analysing psm.tsv file (FragPipe-DIA)")
+      df <- read.csv(file_path, sep = "\t",check.names = FALSE)
+    }
+
   }else{
     print("Input file is Excel type")
     df <- read.xlsx(file_path, check.names = FALSE)
