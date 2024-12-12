55469eab2ca08e29fa344d1a7c23048153ec37aa
crojaram <crojaram@umich.edu>
2024-12-12 18:59:03 -0500

    Adapting LFQ prepr fucntion for report.tsv data frame
    Need to change fucntion's name in the future

--- R/LFQ_Prep.R
+++ R/LFQ_Prep.R
@@ -9,17 +9,45 @@
 #' @examples LFQ_Prep()
 #' @aliases LFQ_Prep
 LFQ_Prep <- function() {
-  #Select Relevant Data
-  selected_data <<- column_selectionLFQ(raw_data)
-  #Rename Certain Columns and Split the Spectrum Files
-  modified_data <<- rename_and_split_spectrum_files(selected_data)
-  #Identify Sample vs Control Files
-  modified_data <<- SampleControl(modified_data)
-  #Annotate Features in the Data
-  modified_data_annotated <<- annotate_features(modified_data)
-  #Parse the FASTA file for later manipulations
-  FASTA<<- parse_fasta(FASTA)
-  #Locate the Start and End Residues for Each Peptide
-  mod_data_fasta_merged<<- locate_startend_res(modified_data_annotated, FASTA)
+  # Detect raw_data type based on finding a specific column name and choose
+  # for the users
+
+  columns_rawdata <- colnames(raw_data)
+  print(columns_rawdata)
+  print(raw_data$`Q Value`)
+  # TRUE (Correct)
+  print("Q Value" %in% columns_rawdata)
+  # False (So the point get converted to a space)
+  print("Q.Value" %in% columns_rawdata)
+
+  if ("Delta Mass" %in% columns_rawdata & "Assigned Modifications" %in% columns_rawdata){
+    # FragPipe DDA
+    print("Extracting columns from psm.tsv file")
+
+  }else if("Q Value" %in% columns_rawdata & "Modified Sequence" %in% columns_rawdata){
+    # FragPipe DIA
+    print("Extracting columns from report.tsv file")
+
+
+  }else{
+    #Proteome Discover
+    #Select Relevant Data
+    selected_data <<- column_selectionLFQ(raw_data)
+    #Rename Certain Columns and Split the Spectrum Files
+    modified_data <<- rename_and_split_spectrum_files(selected_data)
+    #Identify Sample vs Control Files
+    modified_data <<- SampleControl(modified_data)
+    #Annotate Features in the Data
+    modified_data_annotated <<- annotate_features(modified_data)
+    #Parse the FASTA file for later manipulations
+    FASTA<<- parse_fasta(FASTA)
+    #Locate the Start and End Residues for Each Peptide
+    mod_data_fasta_merged<<- locate_startend_res(modified_data_annotated, FASTA)
+  }
+  #print(raw_data$`Modified Sequence`)
+
+
+
+
 }
 
