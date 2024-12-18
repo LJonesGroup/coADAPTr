bcd8c62a697caf4d12336b2b8dd44f53a5f1663a
crojaram <crojaram@umich.edu>
2024-12-18 18:54:14 -0500

    Extracting appropriate columns from DIA report.tsv file

--- R/column_selectionLFQ.R
+++ R/column_selectionLFQ.R
@@ -37,7 +37,27 @@ column_selectionLFQ <- function(df, filetype) {
 
 
   }else if(filetype=="report"){
-    print("DIA Rules!")
+
+    # the peptide sequences (unannotated)
+    # So why not df...it does not work, plus in LFQ_prerp raw data is used as a global environment variable already
+    seq_col <- raw_data$`Stripped Sequence`
+    # the Uniprot ID or master protein accessions
+    acc_col <- raw_data$'Protein Ids'
+    # Modifications
+    mod_col <- raw_data$'FPOP Modifications'
+    # Precursor abundance/intensities
+    pre_col <- raw_data$`Ms1 Area`
+    # The spectrum file IDs
+    spe_col <- raw_data$'Run'
+
+    refined_data <- data.frame(matrix(ncol = 0, nrow = nrow(raw_data)))  # Initialize an empty data frame with the same number of rows as df
+    col_names <- colnames(raw_data)
+
+    refined_data$'Peptide' <- seq_col
+    refined_data$'Protein ID' <- acc_col
+    refined_data$'FPOP Modifications' <- mod_col
+    refined_data$'Intensity' <- pre_col
+    refined_data$'Spectrum' <- spe_col
   }else{
     refined_data <- data.frame(matrix(ncol = 0, nrow = nrow(df)))  # Initialize an empty data frame with the same number of rows as df
     col_names <- colnames(df)
@@ -72,14 +92,10 @@ column_selectionLFQ <- function(df, filetype) {
 
   }
 
-
-
-
-
   # Rename columns
   colnames(refined_data) <- c("Sequence", "Master Protein Accessions", "Modifications", "Precursor Abundance", "Spectrum File")
 
-  print(refined_data)
+  #print(refined_data)
 
   return(refined_data)
 }
